6631143852cd8b6e24015b9827169342e16b8489
Fábio Santos <fabio3268@gmail.com>
2022-08-08 19:16:28 -0300

    Start Data Layer

--- source/App/Web.php
+++ source/App/Web.php
@@ -2,6 +2,8 @@
 
 namespace Source\App;
 
+use Source\Models\User;
+
 class Web
 {
     public function home() : void
@@ -19,6 +21,22 @@ class Web
         include __DIR__ . "/../../themes/web/404.php";
     }
 
-}
+    public function pdoTest(array $data) : void
+    {
+        $model = new User();
+        $users = $model->find()->fetch(true);
+        //var_dump($users);
 
-?>
+        $model = new User();
+        $user = $model->findById(7  );
+        //var_dump($user);
+
+        $model = new User();
+        $user = $model->findByEmail("fabiosantos@ifsul.edu.br");
+        var_dump($user);
+        $user->name = "Fábio";
+        $user->save();
+        var_dump($user);
+    }
+
+}
--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -4,10 +4,26 @@
 /**
  * DATABASE
  */
-define("CONF_DB_HOST", "localhost");
-define("CONF_DB_USER", "root");
-define("CONF_DB_PASS", "");
-define("CONF_DB_NAME", "bd-acme"); // aqui deve ser alterado para o nome do banco de dados
+
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "bd-acme-tarde",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
+
+//define("CONF_DB_HOST", "localhost");
+//define("CONF_DB_USER", "root");
+//define("CONF_DB_PASS", "");
+//define("CONF_DB_NAME", "bd-acme"); // aqui deve ser alterado para o nome do banco de dados
 
 /**
  * PROJECT URLs
